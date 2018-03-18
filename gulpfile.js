cf26e40918fff9e9bf09dec2d893a94c8a91017a
Kirill <kirill.voyshnarovich@gmail.com>
2018-03-18 14:53:06 +0300

    start work with gulp-and-git and gulp stracture

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+
+
+gulp.task('sass', function () {  /*для gulp модуля подключаем задачу под название 'sass' */
+    return gulp.src('./src/styles/*.scss') /*указываем путь откудо взять файл scss,(мы берем его из папки src т. е исходног кода ) */
+      .pipe(sass().on('error', sass.logError))/*Далее говорим через какую задачу провесть под названием sass(это по ходу то что мы задали константу const sass) */
+      .pipe(gulp.dest('./build/styles')); /*здесь указываем в какую папку нам нужно отправить отредактированный файл*/
+  });
+
+
+  gulp.task('pug', function () {
+    return gulp.src('./src/pages/*.pug"')
+    .pipe(pug({
+        // Your options in here. 
+      }))
+      .pipe(gulp.dest('./build')); 
+  });
