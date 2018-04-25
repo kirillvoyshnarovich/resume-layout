c8101145b97729dca81e910e8e4c66015133dfc6
unknown <kirill.voyshnarovich@gmail.com>
2018-04-26 00:59:24 +0300

    correct header in css, add autoprefix-tasck, correct adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 const image = require('gulp-image');
 const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('sass', function () {  /*для gulp модуля подключаем задачу под название 'sass' */
     return gulp.src('./src/styles/*.scss') /*указываем путь откудо взять файл scss,(мы берем его из папки src т. е исходног кода ) */
-      .pipe(sass().on('error', sass.logError))/*Далее говорим через какую задачу провесть под названием sass(это по ходу то что мы задали константу const sass) */
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false})) /*Далее говорим через какую задачу провесть под названием sass(это по ходу то что мы задали константу const sass) */
       .pipe(gulp.dest('./build/styles')); /*здесь указываем в какую папку нам нужно отправить отредактированный файл*/
   });
 
