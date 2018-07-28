01c77c21206ded0e6111ac9dba402c370d1ce728
unknown <kirill.voyshnarovich@gmail.com>
2018-07-28 16:21:15 +0300

    changed menu

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,10 +1,31 @@
+                
+                    //animation for lineDrawing block
 var lineDrawing = anime({
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 900,
+    duration: 500,
     delay: function(el, i) { return i*250},
   });
   
+          //changes visibility for about-me-photo
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  if(window.innerWidth <= 450) {
+    console.log(window.innerWidth);
+
+    var photo = document.querySelector(".about-me-photo");
+
+    var photoSmile = document.querySelector(".about-me-photo-image_smile");
+
+    photo.addEventListener("click", function() {
+
+      photoSmile.classList.toggle("about-me-photo-image_visible")
+
+    })
+  }
+
+})
 
 
